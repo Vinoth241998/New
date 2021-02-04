77e3853f46e3e00ff318939cc765c4b1ea07b26b
VINOTH s <77488556+Vinoth241998@users.noreply.github.com>
2021-02-04 23:20:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res)=> {
+res.send('Hello World!');
+});
+
+app.post('/getIndexes', (req, res)=> {
+    var indexingQuery = `ALTER TABLE ${req.body.tablename} `;
+    const columns  = req.body.columns;
+
+    columns.forEach((element, index) => {
+        if(index === 0) {
+            indexingQuery = indexingQuery + ` ADD INDEX \`idx_${element}\`(\`${element}\`)`;
+        } else {
+            indexingQuery = indexingQuery + `, ADD INDEX \`idx_${element}\`(\`${element}\`)`;
+        }
+    });
+
+    const output = {
+        tableName: req.tableName,
+        indexingQuery: indexingQuery + ';'
+    }
+
+    res.send(output)
+    
+
+
+})
+
+app.listen(8080, ()=> console.log('Server is up and running'));
